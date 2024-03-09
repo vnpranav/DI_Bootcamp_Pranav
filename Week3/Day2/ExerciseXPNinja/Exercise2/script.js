11aab206c8df735dae77f29a47775050c0ac0795
vpranavn <v.pranav.n@gmail.com>
2024-03-09 08:47:42 +0400

    portfolio webpage demo

--- Week3/Day2/ExerciseXPNinja/Exercise2/script.js
+++ Week3/Day2/ExerciseXPNinja/Exercise2/script.js
@@ -1,6 +1,11 @@
 var str1 = prompt("Enter string of numbers seperated by commas: ");
 var nums = str1.split(",").map(Number);
-console.log(nums.join());
+var sum = 0;
+
+for (let num of nums) {
+    sum += num;
+}
+console.log("Sum: ", sum);
 // stuck here
 // how to find sum without loop
 // ._.
