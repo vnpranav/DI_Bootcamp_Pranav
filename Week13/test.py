d4da85b0a64967f452ccac8bffe2589b8b89c364
vnpranav <v.pranav.n@gmail.com>
2024-05-30 18:50:23 +0400

    py py

---
+++ Week13/test.py
@@ -0,0 +1,59 @@
+# age = 20
+# print(f"my age in 123879 years will be {age + 123879}")
+
+#first_name = 'Pranav'
+# last_name = 'Nenooth'
+# print('My full name is: ', first_name,  last_name)
+
+# age = input("How old are you? ")
+# print(f"You are {age} years old")
+# print(type(age))
+
+# num = int(input("Enter a number between 1 and 100: "))
+# if num % 3 == 0 and num % 5 == 0:
+#     print("FizzBuzz")
+# else:
+#     if num % 3 == 0:
+#         print("Fizz")
+#     if num % 5 is 0:
+#         print("Buzz")
+
+# list1 = [5, 10, 15, 20, 25, 50, 20]
+# position = list1.index(20, 0, len(list1))
+# if position != -1:
+#     list1[position] = 200
+# print(list1)
+
+# a_tuple = (10, 20, 30, 40)
+# a1, a2, a3, a4 = a_tuple
+# print(a1,a2,a3,a4)
+
+# number = int(input('Enter multiplication table number: '))
+# for i in range(1,13):
+#     print(f'{i}  *  {number}  =   {number*i}')
+
+# sample_dict = { 
+#    "class":{ 
+#       "student":{ 
+#          "name":"Mike",
+#          "marks":{ 
+#             "physics":70,
+#             "history":80
+#          }
+#       }
+#    }
+# }
+# print(sample_dict['class']['student']['marks']['history'])
+
+# sample_dict = {
+#   "name": "Kelly",
+#   "age":25,
+#   "salary": 8000,
+#   "city": "New york"
+
+# }
+# keys_to_remove = ["name", "salary"]
+# for key in keys_to_remove:
+#     if key in sample_dict.keys():
+#         del sample_dict[key]
+# print(sample_dict)
