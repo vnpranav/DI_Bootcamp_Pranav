c7508ae2899b31aa0cd8e4e50f6f6feb4299cab8
pranav <v.pranav.n@gmail.com>
2024-07-24 21:37:09 +0400

    XP ninja

--- Week18/Day1Exercises/ExerciseXP/people_project/people/info/urls.py
+++ Week18/Day1Exercises/ExerciseXP/people_project/people/info/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
    path('', views.display_person, name='display_person'),
+   path('people/', views.display_people, name='display_people'),
+   path('all_people/', views.display_all_people, name='display_all_people')
 ]
--- Week18/Day1Exercises/ExerciseXP/people_project/people/info/views.py
+++ Week18/Day1Exercises/ExerciseXP/people_project/people/info/views.py
@@ -5,9 +5,44 @@ from django.http import HttpResponse
 name = 'bob smith'
 age = 35
 country = 'usa'
+people = ['bob','martha', 'fabio', 'john']
+
+all_people = [
+  {
+    'id': 1,
+    'name': 'Bob Smith',
+    'age': 35,
+    'country': 'USA'
+  },
+  {
+    'id': 2,
+    'name': 'Martha Smith',
+    'age': 60,
+    'country': 'USA'
+  },
+  {
+    'id': 3,
+    'name': 'Fabio Alberto',
+    'age': 18,
+    'country': 'Italy'
+  },
+  {
+    'id': 4,
+    'name': 'Dietrich Stein',
+    'age': 85,
+    'country': 'Germany'
+  }
+]
 
 def display_person(request):
    person_info = f"Name : {name} \n Age: {age}\n Country: {country}"
    return HttpResponse(person_info)
 
+def display_people(request):
+   persons = "\n".join([p for p in people])
+   return HttpResponse(persons)
 
+def display_all_people(request):
+   sorted_people = sorted(all_people, key=lambda x: x['age'])
+   people_list = "\n".join([f"Name: {person['name']}, Age: {person['age']}, Country: {person['country']}" for person in sorted_people])
+   return HttpResponse(people_list)
