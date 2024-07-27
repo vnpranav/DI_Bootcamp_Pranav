619aefbe6dfe7b8eff108ec3fe93eb1046d713cf
pranav <v.pranav.n@gmail.com>
2024-07-27 10:42:38 +0400

    day 2 part 1

---
+++ Week18/Day2/demo/drf_beginners/posts/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+CATEGORY_CHOICES = (
+   ('Dj', 'Django'),
+   ('Py', 'Python'),
+)
+
+class Post(models.Model):
+
+   title = models.CharField(max_length=100)
+   custom_id = models.IntegerField()
+   category = models.CharField(max_length=3, choices=CATEGORY_CHOICES)
+   publish_date = models.DateTimeField(auto_now_add=True)
+   last_updated = models.DateTimeField(auto_now=True)
+
+   def __str__(self):
+       return self.title
